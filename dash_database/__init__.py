05ccc9ff068b0c1d95190fa30b3f4cff0459f865
ThibTrip <40694343+ThibTrip@users.noreply.github.com>
2019-09-08 11:48:46 +0200

    try running doctest from circleci

--- dash_database/__init__.py
+++ dash_database/__init__.py
@@ -227,11 +227,3 @@ class DashDatabase():
         """Create representation for DashDabase (show location of database)."""
         repr_str = f'DashDatabase at "{self.db.filename}"'
         return repr_str
-    
-# # Test code blocks in docstrings using doctest
-
-# In[3]:
-
-if __name__ == "__main__":        
-    import doctest
-    doctest.testmod(raise_on_error = True, verbose = False)
